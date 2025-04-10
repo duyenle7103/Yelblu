244a8bc9af9dc6bfd4e6cf1a046a7bcc4ba3897a
duyen.le7103 <duyen.le7103@hcmut.edu.vn>
2025-04-10 21:21:33 +0700

    Add onboarding

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,49 +1,52 @@
 import { router } from "expo-router"
-import { SafeAreaView, Text, TouchableOpacity, View, Image } from "react-native"
+import { SafeAreaView, Text, TouchableOpacity, View, Image, Dimensions } from "react-native"
 import Swiper from "react-native-swiper"
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 const onboarding = () => {
     return (
         <Swiper loop={false} showsPagination={true}>
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
-            <Image
-                source={require('./assets/images/Component0.png')}
-                style={{ width: 100, height: 100 }}
-            />
+                <Image
+                    source={require('../assets/images/Component0.png')}
+                    style={{ width: screenWidth, height: screenHeight }}
+                />
             </SafeAreaView>
 
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
-            <Image
-                source={require('./assets/images/Component1.png')}
-                style={{ width: 100, height: 100 }}
-            />
+                <Image
+                    source={require('../assets/images/Component1.png')}
+                    style={{ width: screenWidth, height: screenHeight }}
+                />
             </SafeAreaView>
 
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
-            <Image
-                source={require('./assets/images/Component2.png')}
-                style={{ width: 100, height: 100 }}
-            />
+                <Image
+                    source={require('../assets/images/Component2.png')}
+                    style={{ width: screenWidth, height: screenHeight }}
+                />
             </SafeAreaView>
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
-            <Image
-                source={require('./assets/images/Component3.png')}
-                style={{ width: 100, height: 100 }}
-            />
+                <Image
+                    source={require('../assets/images/Component3.png')}
+                    style={{ width: screenWidth, height: screenHeight }}
+                />
             </SafeAreaView>
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
-            <Image
-                source={require('./assets/images/Component4.png')}
-                style={{ width: 100, height: 100 }}
-            />
+                <Image
+                    source={require('../assets/images/Component4.png')}
+                    style={{ width: screenWidth, height: screenHeight }}
+                />
             </SafeAreaView>
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
-            <Image
-                source={require('./assets/images/Component5.png')}
-                style={{ width: 100, height: 100 }}
-            />
+                <Image
+                    source={require('../assets/images/Component5.png')}
+                    style={{ width: screenWidth, height: screenHeight }}
+                />
             </SafeAreaView>
-            
+
         </Swiper>
     )
 }
