c530b9e121cfb05a885a75732f853ceb314a29e4
duyen.le7103 <duyen.le7103@hcmut.edu.vn>
2025-04-10 13:52:27 +0700

    3

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -7,39 +7,39 @@ const onboarding = () => {
         <Swiper loop={false} showsPagination={true}>
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
             <Image
-                source={require('./assets/image/Component0.png')}
+                source={require('./assets/images/Component0.png')}
                 style={{ width: 100, height: 100 }}
             />
             </SafeAreaView>
 
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
             <Image
-                source={require('./assets/image/Component1.png')}
+                source={require('./assets/images/Component1.png')}
                 style={{ width: 100, height: 100 }}
             />
             </SafeAreaView>
 
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
             <Image
-                source={require('./assets/image/Component2.png')}
+                source={require('./assets/images/Component2.png')}
                 style={{ width: 100, height: 100 }}
             />
             </SafeAreaView>
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
             <Image
-                source={require('./assets/image/Component3.png')}
+                source={require('./assets/images/Component3.png')}
                 style={{ width: 100, height: 100 }}
             />
             </SafeAreaView>
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
             <Image
-                source={require('./assets/image/Component4.png')}
+                source={require('./assets/images/Component4.png')}
                 style={{ width: 100, height: 100 }}
             />
             </SafeAreaView>
             <SafeAreaView className="flex-1 justify-center items-center bg-white">
             <Image
-                source={require('./assets/image/Component5.png')}
+                source={require('./assets/images/Component5.png')}
                 style={{ width: 100, height: 100 }}
             />
             </SafeAreaView>
